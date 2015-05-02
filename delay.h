9da8227bbda215d69548a9fd206b205b880376a7
smallbal <605660711@qq.com>
2015-05-02 17:51:19 +0800

    延时函数头文件

---
+++ delay.h
@@ -0,0 +1,13 @@
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#ifndef UCHAR_&_UINT
+#define UCHAR_&_UINT
+#define uchar unsigned char 
+#define uint unsigned int 
+#endif
+
+void delayms(uchar);
+void delay(uchar);
+
+#endif
